eedc500e4eee73c0c28cdf20c4876a0e6eb653e7
shahalihridoy <shahalihridoy@gmail.com>
2019-07-06 01:38:25 +0600

    Updates

---
+++ precache-manifest.543a548c40e32ab0b0a8863749d71664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3287f8ef45bcb09fc2a767bdd6b213a",
+    "url": "/fiverr-chat-box/index.html"
+  },
+  {
+    "revision": "65e0db1e83edf3539aa8",
+    "url": "/fiverr-chat-box/static/css/2.5349a374.chunk.css"
+  },
+  {
+    "revision": "628392a8bae07f593a27",
+    "url": "/fiverr-chat-box/static/css/main.ddc5a037.chunk.css"
+  },
+  {
+    "revision": "65e0db1e83edf3539aa8",
+    "url": "/fiverr-chat-box/static/js/2.189846c2.chunk.js"
+  },
+  {
+    "revision": "628392a8bae07f593a27",
+    "url": "/fiverr-chat-box/static/js/main.5eb3a53a.chunk.js"
+  },
+  {
+    "revision": "232a410305cc42d88089",
+    "url": "/fiverr-chat-box/static/js/runtime~main.dff58b98.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/fiverr-chat-box/precache-manifest.543a548c40e32ab0b0a8863749d71664.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fiverr-chat-box/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
